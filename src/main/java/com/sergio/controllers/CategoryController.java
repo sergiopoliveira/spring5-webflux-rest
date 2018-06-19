0d10dc378b0f0dceaad80c63e8ec65002285eea4
sergiopoliveira <sergiopoliveira@outlook.com>
2018-06-19 11:25:58 +0100

    Category Controller

--- src/main/java/com/sergio/controllers/CategoryController.java
+++ src/main/java/com/sergio/controllers/CategoryController.java
@@ -1,5 +1,32 @@
 package com.sergio.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sergio.domain.Category;
+import com.sergio.repository.CategoryRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class CategoryController {
+
+	private final CategoryRepository categoryRepository;
+
+	public CategoryController(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
+	@GetMapping("/api/v1/categories")
+	Flux<Category> list(){
+		return categoryRepository.findAll();
+	}
+	
+	@GetMapping("/api/v1/categories/{id}")
+	Mono<Category> getById(@PathVariable String id) {
+		return categoryRepository.findById(id);
+	}
 
 }
